a2b2040fa3da1142d08a30c75771259cf6d2c3b6
gio <giovag1995@gmail.com>
2019-06-18 13:44:28 -0400

    lista desplegable para el caso #2 funcionando correctamente

--- app/src/main/java/com/teghpp/Caso2.java
+++ app/src/main/java/com/teghpp/Caso2.java
@@ -1,28 +1,25 @@
 package com.teghpp;
 
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ExpandableListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
-
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Caso2 extends Fragment {
-    ListView listView;
-    private Context context;
+
+    ExpListViewAdapterWithCheckbox listAdapter;
+    ExpandableListView expListView;
+    ArrayList<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -35,31 +32,103 @@ public class Caso2 extends Fragment {
     @Override
     public void onActivityCreated(Bundle state) {
         super.onActivityCreated(state);
-        listView = (ListView) getView().findViewById(R.id.listview1);
+        // get the listview
+        expListView = (ExpandableListView) getView().findViewById(R.id.lvExp);
 
+        // preparing list data
+        prepareListData();
 
-        ArrayList<String> arrayList = new ArrayList<>();
+        listAdapter = new ExpListViewAdapterWithCheckbox(getContext(), listDataHeader, listDataChild);
 
-        arrayList.add("1.   TANN");
-        arrayList.add("2.   Taponamiento uterino");
-        arrayList.add("2.1.   Balon de Bakri");
-        arrayList.add("2.2   Balon Artesanal");
-        arrayList.add("3.   Sutura hemostatica de B-Lynch");
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
 
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
 
-        ArrayAdapter arrayAdapter;
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1, arrayList);
-
-
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getActivity().getApplicationContext(), String.valueOf(position), Toast.LENGTH_SHORT).show();
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getActivity().getApplicationContext(), String.valueOf(groupPosition), Toast.LENGTH_SHORT).show();
+                /** Toast.makeText(getApplicationContext(),
+                 listDataHeader.get(groupPosition) + " Expanded",
+                 Toast.LENGTH_SHORT).show();*/
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                /**Toast.makeText(getApplicationContext(),
+                 listDataHeader.get(groupPosition) + " Collapsed",
+                 Toast.LENGTH_SHORT).show();*/
 
             }
         });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                /** Toast.makeText(
+                 getApplicationContext(),
+                 listDataHeader.get(groupPosition)
+                 + " : "
+                 + listDataChild.get(
+                 listDataHeader.get(groupPosition)).get(
+                 childPosition), Toast.LENGTH_SHORT)
+                 .show();*/
+                return false;
+            }
+        });
+
+
     }
 
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("TANN");
+        listDataHeader.add("Taponamiento uterino:");
+        listDataHeader.add("Sutura hemostatctica de B-Lynch");
+
+
+        // Adding child data
+        List<String> taponamiento = new ArrayList<String>();
+        taponamiento.add("Balon de Bakri");
+        taponamiento.add("Balon de artesanal ");
+
+        List<String> tann = new ArrayList<String>();
+        tann.add("texto de informacion para este caso");
+
+        List<String> sutura = new ArrayList<String>();
+        sutura.add("texto de informacion para este caso");
+
+
+        listDataChild.put(listDataHeader.get(0), tann); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), taponamiento);
+        listDataChild.put(listDataHeader.get(2), sutura);
+
+    }
 }
+
+
--- app/src/main/java/com/teghpp/Caso3.java
+++ app/src/main/java/com/teghpp/Caso3.java
@@ -122,38 +122,22 @@ public class Caso3 extends Fragment {
         listDataChild = new HashMap<String, List<String>>();
 
         // Adding child data
-        listDataHeader.add("Top 250");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
+        listDataHeader.add("Histerectomía obstétrica");
+        listDataHeader.add("Cirugía de control de daños ");
+
 
         // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
+        List<String> histerectomia = new ArrayList<String>();
+        histerectomia.add("texto de informacion para este caso texto de informacion para este caso texto de informacion para este caso texto de informacion para este caso texto de informacion para este caso texto de informacion para este caso");
 
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
 
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
+        List<String> cirugia = new ArrayList<String>();
+        cirugia.add("texto de informacion para este caso");
 
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
+
+        listDataChild.put(listDataHeader.get(0), histerectomia); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), cirugia);
+
     }
 }
 
